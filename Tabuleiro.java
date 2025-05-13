4d1608f7c54f844f10dced55c38c9c0ebf327eb4
Pedro Mikhael <pedromikhaelsouza@gmail.com>
2025-05-12 21:20:39 -0300

    Todas as casas especiais foram criadas

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,7 +1,12 @@
 import classeJogadores.JogadorAzarado;
+import classeJogadores.JogadorNormal;
+import classeJogadores.JogadorSortudo;
 import classeJogadores.Jogadores;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Tabuleiro {
 	protected ArrayList<Jogadores> jogadores;
@@ -34,6 +39,61 @@ public class Tabuleiro {
 			jogador.setPerdeProximaJogada(true);
 		}
 		
+		// Casas que fazem com que o jogador mude de tipo
+		if (posicao == 13) {
+			Random random = new Random();
+			Scanner teclado = new Scanner(System.in);
+			
+			System.out.println("Escolha a carta surpresa: ");
+			System.out.println("Escolha entre 1, 2 ou 3");
+			int numero = teclado.nextInt();
+			
+			// Obter a cor do jogador atual
+			String corJogador = jogador.getCor();
+			
+			if (numero == 1) {
+				jogador.mudarTipoJogador(new JogadorAzarado(corJogador));
+				System.out.println("Jogador mudou para o tipo Azarado");
+			} else if (numero == 2) {
+				jogador.mudarTipoJogador(new JogadorSortudo(corJogador));
+				System.out.println("Jogador mudou para o tipo Sortudo");
+			} else if (numero == 3) {
+				jogador.mudarTipoJogador(new JogadorNormal(corJogador));
+				System.out.println("Jogador mudou para o tipo Normal");
+			}
+			
+		}
 		
+		
+		
+		//Casas em que o jogador ganha 3 posicoes, se nao for JogadorAzarado
+		if (posicao == 5 || posicao == 15 || posicao == 30) {
+			if (!(jogador instanceof JogadorAzarado)) {
+				jogador.setPosicaoTabuleiro(jogador.getPosicaoTabuleiro() + 3);
+			}
+		}
+		
+		//Casas magicas, troca de posição com o ultimo
+		if (posicao == 20 || posicao == 35) {
+			int posicaoUltimoJogador = -1;
+			int indiceUltimoJogador = -1;
+			
+			// Encontrar o jogador que está na posição mais avançada
+			for(int i = 0; i < jogadores.size(); i++) {
+				if(jogadores.get(i).getPosicaoTabuleiro() > posicaoUltimoJogador && 
+				   jogadores.get(i) != jogador) {
+					posicaoUltimoJogador = jogadores.get(i).getPosicaoTabuleiro();
+					indiceUltimoJogador = i;
+				}
+			}
+			
+			if(indiceUltimoJogador != -1) {
+				int posicaoAtual = jogador.getPosicaoTabuleiro();
+				
+				
+				jogador.setPosicaoTabuleiro(posicaoUltimoJogador);
+				jogadores.get(indiceUltimoJogador).setPosicaoTabuleiro(posicaoAtual);
+			}
+		}	
 	}
 }
